502ae0193c42e8b84d87a9aa9dc493b294f47617
Aurelie <aurelie@recolytic.com>
2025-09-30 16:50:05 +0200

    Fix failed fetch

--- src/app/archives/page.tsx
+++ src/app/archives/page.tsx
@@ -3,6 +3,9 @@ import Artists from '@/app/artists/artists'
 
 const ArtistsPage = async () => {
   const artists = await getArtists()
+  if (artists.length === 0) {
+    return <p>No artists found or failed to fetch.</p>
+  }
   return <Artists artists={artists} />
 }
 
